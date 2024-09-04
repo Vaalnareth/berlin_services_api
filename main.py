b5c4b8725798551d6ff29a1056c337a432188592
Vaalnareth <157876989+Vaalnareth@users.noreply.github.com>
2024-09-04 10:38:53 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from sqlmodel import SQLModel
 from app.db import engine
 from app.routes import router
+import uvicorn
 
 app = FastAPI()
 
@@ -14,3 +15,6 @@ app.include_router(router)
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Service API!"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
